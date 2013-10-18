52ddd92f92863e6a1e38ee2c21a61e157b68dd35
clausd <dee@classy.dk>
2013-10-18 13:50:52 +0200

    y u no commit

--- browser/1993.js
+++ browser/1993.js
@@ -1,12 +1,11 @@
-;(function( $ ){
-  // the converse of load...
-  $.fn.save = function(save_to_url, callback) {
-    $.post(save_to_url, $(this).html(), callback);
-  };
-  $.fn.delete = function(save_to_url, callback) {
-    $
-  };
-  $.fn.put = function(save_to_url, callback) {
-    
+; (function($) {
+    // the converse of load...
+    $.fn.save = function(save_to_url, callback) {
+        $.post(save_to_url, $(this).html(), callback);
+    };
+    $.fn.delete = function(save_to_url, callback) {
+        $
+    };
+    $.fn.put = function(save_to_url, callback) {
   };
 })( jQuery );
