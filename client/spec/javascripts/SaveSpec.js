ffc22a5db053b76709c9dade697f840de5dedb9b
clausd <dee@classy.dk>
2013-10-18 17:00:48 +0200

    removed boilerplate - test now loads proper libs

---
+++ client/spec/javascripts/SaveSpec.js
@@ -0,0 +1,11 @@
+describe("1993", function() {
+  beforeEach(function() {
+    // something
+  });
+
+  it("should save element on save", function() {
+    // load a fixture
+    expect(typeof $('div').save('tosomeurl')).toBeTruthy();
+  });
+
+});
